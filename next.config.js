c22ae9b822777444ad4590065341caf778b020dc
Sahith2607 <sahithreddy0426@gmail.com>
2025-09-11 11:33:06 +0530

    updates with some

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // Disable ESLint during production builds to avoid missing plugin errors
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   images: {
     domains: ['picsum.photos'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'i.ibb.co',
-        pathname: '/**',
+        pathname: '/**', // allow all images from i.ibb.co
       },
     ],
   },
