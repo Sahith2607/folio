5fde9734f126d3a6edfdf3516ac8b6030912869e
Sahith2607 <sahithreddy0426@gmail.com>
2025-09-11 10:48:19 +0530

    Stop tracking .next build artifacts

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"K9ejQGZI2FCHiAG04/bhNKBZnB/yJDrwkHABMXj1zCw=\"\n}"
--- .next/static/chunks/src_app_page_tsx_1f0f2f98._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_bc0ca602._.js",
-  "static/chunks/src_bc5d89a6._.js"
-],
-    source: "dynamic"
-});
